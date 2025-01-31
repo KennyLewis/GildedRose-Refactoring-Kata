0c283bdbfd2c00a0a5967ce04ba6f7bd68f20560
Kenny Lewis <kl569@nau.edu>
2025-01-30 18:20:52 -0700

    Implement ItemProxy.cs
    Following along with https://app.pluralsight.com/ilx/video-courses/58709b4a-e2a1-4066-b777-3fa946329390/187b9260-49e5-4f8b-bcb9-fa00b22d02b0/67331def-f997-44ca-9ea8-39e6024136c4

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -11,79 +11,67 @@ public class GildedRose
         this.Items = Items;
     }
 
-    public void UpdateQuality()
+    public void UpdateQuality(ItemProxy item)
     {
-        for (var i = 0; i < Items.Count; i++)
+        if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
         {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            if (item.Name != "Sulfuras, Hand of Ragnaros")
             {
-                if (Items[i].Quality > 0)
-                {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
-                }
+                item.DecrementQuality();
             }
-            else
+        }
+        else
+        {
+            item.IncrementQuality();
+
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (Items[i].Quality < 50)
+                if (item.SellIn < 11)
                 {
-                    Items[i].Quality = Items[i].Quality + 1;
+                    item.IncrementQuality();
+                }
 
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-                    }
+                if (item.SellIn < 6)
+                {
+                    item.IncrementQuality();
                 }
             }
 
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-            {
-                Items[i].SellIn = Items[i].SellIn - 1;
-            }
+        }
 
-            if (Items[i].SellIn < 0)
+        if (item.Name != "Sulfuras, Hand of Ragnaros")
+        {
+            item.DecrementSellIn();
+        }
+
+        if (item.SellIn < 0)
+        {
+            if (item.Name != "Aged Brie")
             {
-                if (Items[i].Name != "Aged Brie")
+                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        item.DecrementQuality();
                     }
                 }
                 else
                 {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
+                    item.ResetQuality();
                 }
             }
+            else
+            {
+                item.IncrementQuality();
+            }
+        }
+    }
+
+    public void UpdateQuality()
+    {
+        for (var i = 0; i < Items.Count; i++)
+        {
+            UpdateQuality(new ItemProxy(Items[i]));
         }
     }
 }
---
+++ GildedRose/ItemProxy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseKata
+{
+    public class ItemProxy
+    {
+        private readonly Item _item;
+
+        public ItemProxy(Item item)
+        {
+            _item = item;
+        }
+
+        public string Name => _item.Name;
+        public int SellIn => _item.SellIn;
+        public int Quality => _item.Quality;
+
+        public void IncrementQuality()
+        {
+            // The Quality of an item is never more than 50
+            if (_item.Quality < 50)
+            {
+                _item.Quality++;
+            }
+        }
+
+        public void DecrementQuality()
+        {
+            // The Quality of an item is never negative
+            if (_item.Quality > 0)
+            {
+                _item.Quality--;
+            }
+        }
+
+        public void ResetQuality()
+        {
+            _item.Quality = 0;
+        }
+
+        public void DecrementSellIn()
+        {
+            _item.SellIn--;
+        }
+    }
+}
