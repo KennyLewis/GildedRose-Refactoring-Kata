c4821638b7f2950dfc3f0559a183a10dd3c66a98
Kenny Lewis <kl569@nau.edu>
2025-02-18 14:09:05 -0700

    Comment out code that was refactored

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -12,76 +12,76 @@ public class GildedRose
         this.Items = Items;
     }
 
-    public void UpdateSulfuras(ItemProxy item)
-    {
-        // "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-    }
+    //public void UpdateSulfuras(ItemProxy item)
+    //{
+    //    "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+    //}
 
-    public void UpdateAgedBrie(ItemProxy item)
-    {
-        item.IncrementQuality();
-        item.DecrementSellIn();
+    //public void UpdateAgedBrie(ItemProxy item)
+    //{
+    //    item.IncrementQuality();
+    //    item.DecrementSellIn();
 
-        // "Aged Brie" actually increases in Quality the older it gets
-        if (item.SellIn < 0)
-        {
-            item.IncrementQuality();
-        }
-    }
+    //    "Aged Brie" actually increases in Quality the older it gets
+    //    if (item.SellIn < 0)
+    //    {
+    //        item.IncrementQuality();
+    //    }
+    //}
 
-    public void UpdateBackstagePasses(ItemProxy item)
-    {
-        // "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches
-        item.IncrementQuality();
+    //public void UpdateBackstagePasses(ItemProxy item)
+    //{
+    //    "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches
+    //    item.IncrementQuality();
 
-        // Quality increases by 2 when there are 10 days or less
-        if (item.SellIn <= 10)
-        {
-            item.IncrementQuality();
-        }
+    //    Quality increases by 2 when there are 10 days or less
+    //    if (item.SellIn <= 10)
+    //    {
+    //        item.IncrementQuality();
+    //    }
 
-        // Quality increases by 3 when there are 5 days or less
-        if (item.SellIn <= 5)
-        {
-            item.IncrementQuality();
-        }
+    //    Quality increases by 3 when there are 5 days or less
+    //    if (item.SellIn <= 5)
+    //    {
+    //        item.IncrementQuality();
+    //    }
 
-        item.DecrementSellIn();
+    //    item.DecrementSellIn();
 
-        // but Quality drops to 0 after the concert
-        if (item.SellIn < 0)
-        {
-            item.ResetQuality();
-        }
-    }
+    //    but Quality drops to 0 after the concert
+    //    if (item.SellIn < 0)
+    //    {
+    //        item.ResetQuality();
+    //    }
+    //}
 
-    public void UpdateConjuredItem(ItemProxy item)
-    {
-        // "Conjured" items degrade in Quality twice as fast as normal items
-        item.DecrementQuality();
-        item.DecrementQuality();
+    //public void UpdateConjuredItem(ItemProxy item)
+    //{
+    //    "Conjured" items degrade in Quality twice as fast as normal items
+    //    item.DecrementQuality();
+    //    item.DecrementQuality();
 
-        item.DecrementSellIn();
+    //    item.DecrementSellIn();
 
-        // Once the sell by date has passed, Quality degrades twice as fast
-        if (item.SellIn < 0)
-        {
-            item.DecrementQuality();
-            item.DecrementQuality();
-        }
-    }
+    //    Once the sell by date has passed, Quality degrades twice as fast
+    //    if (item.SellIn < 0)
+    //    {
+    //        item.DecrementQuality();
+    //        item.DecrementQuality();
+    //    }
+    //}
 
-    public void UpdateNormalItem(ItemProxy item)
-    {
-        item.DecrementQuality();
-        item.DecrementSellIn();
+    //public void UpdateNormalItem(ItemProxy item)
+    //{
+    //    item.DecrementQuality();
+    //    item.DecrementSellIn();
 
-        // Once the sell by date has passed, Quality degrades twice as fast
-        if (item.SellIn < 0)
-        {
-            item.DecrementQuality();
-        }
-    }
+    //    Once the sell by date has passed, Quality degrades twice as fast
+    //    if (item.SellIn < 0)
+    //    {
+    //        item.DecrementQuality();
+    //    }
+    //}
 
     //public void UpdateQuality(ItemProxy item)
     //{
