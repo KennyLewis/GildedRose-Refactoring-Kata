0f5541aa6e5ab1d646a444e69c8dec92e633c474
Kenny Lewis <kl569@nau.edu>
2025-02-18 13:59:08 -0700

    Create AgedBrieRule.cs

---
+++ GildedRose/Rules/AgedBrieRule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseKata.Rules
+{
+    public class AgedBrieRule : RuleBase
+    {
+        public override void AdjustQuality(ItemProxy item)
+        {
+            item.IncrementQuality();
+        }
+
+        public override void AdjustQualityForNegativeSellIn(ItemProxy item)
+        {
+            item.IncrementQuality();
+        }
+
+        public override bool IsMatch(ItemProxy item)
+        {
+            return item.Name == "Aged Brie";
+        }
+    }
+}
