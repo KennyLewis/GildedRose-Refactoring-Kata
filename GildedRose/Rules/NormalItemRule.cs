5ca4386baf54cba16bd3379b58806902572ba9fb
Kenny Lewis <kl569@nau.edu>
2025-02-18 13:59:05 -0700

    Create NormalItemRule.cs

---
+++ GildedRose/Rules/NormalItemRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseKata.Rules
+{
+    public class NormalItemRule : RuleBase
+    {
+        public override void AdjustQuality(ItemProxy item)
+        {
+            item.DecrementQuality();
+        }
+
+        public override void AdjustQualityForNegativeSellIn(ItemProxy item)
+        {
+            item.DecrementQuality();
+        }
+
+        public override bool IsMatch(ItemProxy item)
+        {
+            return true; // This is the default and always true
+        }
+    }
+}
